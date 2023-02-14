49542545a056e917c0540a301b8e664129b597c5
m.kato <mkatochoco1013@gmail.com>
2023-02-14 14:11:15 +0900

    [Modify] app追記、微修正

--- security_group.tf
+++ security_group.tf
@@ -1,7 +1,10 @@
 #------------------
 # Security Group
 #------------------
+
+#===================
 # web security group
+#===================
 resource "aws_security_group" "web_sg" {
   # nameとdescriptionは指定。空欄だとよくわからない文字列が入る。
   name        = "${var.project}-${var.environment}-web-sg"
@@ -37,16 +40,18 @@ resource "aws_security_group_rule" "web_in_https" {
 }
 
 resource "aws_security_group_rule" "web_out_tcp3000" {
-  security_group_id        = aws_security_group.web_sg.id
-  type                     = "egress" # アウトバウンド
-  protocol                 = "tcp"
-  from_port                = 3000 # node.js用ポート
-  to_port                  = 3000
+  security_group_id = aws_security_group.web_sg.id
+  type              = "egress" # アウトバウンド
+  protocol          = "tcp"
+  from_port         = 3000 # node.js用ポート
+  to_port           = 3000
   # appへ出るため、["0.0.0.0/0"]はNG。
   source_security_group_id = aws_security_group.app_sg.id
 }
 
+#===================
 # app security group
+#===================
 resource "aws_security_group" "app_sg" {
   name        = "${var.project}-${var.environment}-app-sg"
   description = "application server role security group"
@@ -76,7 +81,7 @@ resource "aws_security_group_rule" "app_out_http" {
   to_port           = 80
   # 外部への接続は「source_security_group_id」ではなくプレフィックスリストを使用
   # S3へ接続するため以下の通り記載。
-  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+  prefix_list_ids = [data.aws_prefix_list.s3_pl.id]
 }
 
 resource "aws_security_group_rule" "app_out_https" {
@@ -88,7 +93,18 @@ resource "aws_security_group_rule" "app_out_https" {
   prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
 }
 
+resource "aws_security_group_rule" "app_out_tcp3306" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
+#===================
 # operation and management security group（運用管理）
+#===================
 resource "aws_security_group" "opmng_sg" {
   name        = "${var.project}-${var.environment}-opmng-sg"
   description = "operation and management role security group"
@@ -119,7 +135,7 @@ resource "aws_security_group_rule" "opmng_in_tcp3000" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "opmng_in_http" {
+resource "aws_security_group_rule" "opmng_out_http" {
   security_group_id = aws_security_group.opmng_sg.id
   type              = "egress"
   protocol          = "tcp"
@@ -128,7 +144,7 @@ resource "aws_security_group_rule" "opmng_in_http" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "opmng_in_https" {
+resource "aws_security_group_rule" "opmng_out_https" {
   security_group_id = aws_security_group.opmng_sg.id
   type              = "egress"
   protocol          = "tcp"
@@ -137,7 +153,9 @@ resource "aws_security_group_rule" "opmng_in_https" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+#===================
 # db security group
+#===================
 resource "aws_security_group" "db_sg" {
   name        = "${var.project}-${var.environment}-db-sg"
   description = "database role security group"
