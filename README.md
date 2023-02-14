ee61e4dc7f8dc5915e26aea01c2543cc66cb8eb8
m.kato <mkatochoco1013@gmail.com>
2023-02-14 14:02:44 +0900

    [Add] セキュリティグループ追加

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ Terraformを使用して、AWSの環境構築練習のためのソース
 - こっちでもいい
 `terraform apply --auto-approve`
 ## プロバイダ関連
-main.tf
+`main.tf`
 ## VPC関連
-network.tf
+`network.tf`
 
 ### サブネット
 1a,1cでそれぞれpublicとprivateを作成。
@@ -34,3 +34,22 @@ publicとprivate用の2つ作成。
 VPCへインターネットゲートウェイの設定
 
 publicのルートテーブルと接続する
+
+## セキュリティグループ
+### 以下の4つ作成
+- WEB
+    - HTTP、HTTPSが入る
+    - TCP3000が出る
+- AP
+    - TCP3000が入る
+    - HTTP,HTTPSがS3へ出るのとMYSQL向けポートTCP3306が出る
+- DB
+    - MYSQL向けのポート TPC3306が入る
+- 運用管理
+    - SSH、TCP3000が入る
+    - HTTP、HTTPSが出る
+
+## プレフィックスリスト
+外部から取り込む系は以下ファイルに定義
+
+`data.tf`
---
+++ security_group.tf
@@ -0,0 +1,160 @@
+#------------------
+# Security Group
+#------------------
+# web security group
+resource "aws_security_group" "web_sg" {
+  # nameとdescriptionは指定。空欄だとよくわからない文字列が入る。
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web front role security group"
+  # 同一ディレクトリにtfファイルがある場合、vpcも参照可能。
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-web-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "web_in_http" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress" # インバウンド
+  # ここは、tcp,udpなどのトランスポート層やネットワーク層のプロトコルを記載。
+  # httpとかsshはNG。
+  protocol    = "tcp"
+  from_port   = 80 # httpのポート
+  to_port     = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_in_https" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443 # httpsのポート
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_out_tcp3000" {
+  security_group_id        = aws_security_group.web_sg.id
+  type                     = "egress" # アウトバウンド
+  protocol                 = "tcp"
+  from_port                = 3000 # node.js用ポート
+  to_port                  = 3000
+  # appへ出るため、["0.0.0.0/0"]はNG。
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+# app security group
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project}-${var.environment}-app-sg"
+  description = "application server role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "app_in_tcp3000" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "app_out_http" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  # 外部への接続は「source_security_group_id」ではなくプレフィックスリストを使用
+  # S3へ接続するため以下の通り記載。
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "app_out_https" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+# operation and management security group（運用管理）
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.environment}-opmng-sg"
+  description = "operation and management role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-opmng-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22 # sshのポート
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000 # tcpのポート
+  to_port           = 3000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_http" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80 # sshのポート
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_https" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443 # httpsのポート
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# db security group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "database role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "db_in_tcp3000" {
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306 # mysql
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.app_sg.id # appから入る
+}
