66d515f67689fb3305c68b61e72b6cf62c033a49
m.kato <mkatochoco1013@gmail.com>
2023-02-15 11:22:02 +0900

    [Add] EC2追加

--- appserver.tf
+++ appserver.tf
@@ -12,3 +12,24 @@ resource "aws_key_pair" "keypair" {
     Env     = var.environment
   }
 }
+
+#===================
+# EC2 Instance
+#===================
+resource "aws_instance" "app_server" {
+  # data.tf参照
+  ami           = data.aws_ami.app.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnet_1a.id
+  # publicIPを設定
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.app_sg.id, aws_security_group.opmng_sg.id]
+  key_name                    = aws_key_pair.keypair.key_name
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-ec2"
+    Project = var.project
+    Env     = var.environment
+    # アプリケーションサーバー
+    Type = "app"
+  }
+}
