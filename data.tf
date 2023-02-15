47d39f35b03f0396030c93b5884d84d5d656f090
m.kato <mkatochoco1013@gmail.com>
2023-02-15 10:34:15 +0900

    [Add] AMI情報記載

--- data.tf
+++ data.tf
@@ -2,3 +2,28 @@ data "aws_prefix_list" "s3_pl" {
   # 環境によってリージョンは異なるので「*」
   name = "com.amazonaws.*.s3"
 }
+
+data "aws_ami" "app" { # アプリケーション用だからapp
+  # 最新のものを選択
+  most_recent = true
+  # 自分自身が登録したものと、Amazon公式のものを検索
+  owners = ["self", "amazon"]
+
+  filter {
+    name = "name"
+    # aws ec2 describe-images --image-ids インスタンスID コマンドで取得。
+    # Name属性を記載
+    # 途中の「日付.0」の箇所を「*」にし、全体を取得。
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
