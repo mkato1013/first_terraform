f8ec6693674e7f4883f293569976086c9c5e7191
m.kato <mkatochoco1013@gmail.com>
2023-02-15 10:34:33 +0900

    [Add] キーペア情報記載

---
+++ appserver.tf
@@ -0,0 +1,14 @@
+#===================
+# key pair
+#===================
+resource "aws_key_pair" "keypair" {
+  key_name = "${var.project}-${var.environment}-keypair"
+  # 公開鍵をfileとして読みこむ
+  public_key = file("./src/tastylog-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Project = var.project
+    Env     = var.environment
+  }
+}
